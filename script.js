6dc755daffeece21a4901ba98c370a93a15f1c75
lilberick <lilberick@gmail.com>
2023-04-08 02:58:31 -0500

    Codigo LaTeX extraído del csv carga normal

--- script.js
+++ script.js
@@ -32,10 +32,17 @@ function cronometro(cronometro,ntimer){
   }
 }
 //Verifica si la respuesta es correcta
-function verificarRespuesta(pregunta, solucion, mensajeID){
+/*function verificarRespuesta(pregunta, solucion, mensajeID){
 	var respuesta=document.querySelector('input[name="'+pregunta+'"]:checked').value;
 	var mensaje="";
 	if(respuesta==solucion){mensaje="¡Correcto!";}
+	else{mensaje="Incorrecto";}
+	document.getElementById(mensajeID).innerHTML = mensaje;
+}*/
+function verificarRespuesta(solucion, mensajeID){
+	var mensaje="";
+	var alternativa= document.querySelector('input[name="respuesta"]:checked').value;
+	if(alternativa==solucion){mensaje="¡Correcto!";}
 	else{mensaje="Incorrecto";}
 	document.getElementById(mensajeID).innerHTML = mensaje;
 }
@@ -46,3 +53,59 @@ silabos.forEach((silabo) => {
 		silabo.classList.toggle('active');
 	});
 });
+//Cambiar problema
+function CambiarProblema(){
+  // Generar números aleatorios
+  var num1 = Math.floor(Math.random() * 10) + 1;
+  var num2 = Math.floor(Math.random() * 10) + 1;
+  // Generar operador aleatorio
+  var operador = Math.random() < 0.5 ? '+' : '-';
+  // Calcular resultado
+  var resultado = operador == '+' ? num1 + num2 : num1 - num2;
+  // Actualizar HTML con el problema generado
+  document.getElementById('cambiarProblema').innerHTML = num1 + ' ' + operador + ' ' + num2 + ' = ' + resultado;
+}
+/*function cargarProblemas(){
+	const archivo = new h5web.File('mi_archivo.h5', 'r');
+	const dataset = archivo.dataset('mi_dataset');
+	const data = dataset.read();
+	const enunciado = data[0][0];
+	const respuesta_correcta = data[0][1];
+	const opciones = data[0].slice(2);
+	const problemaDiv = document.getElementById('problema');
+	problemaDiv.innerHTML = `
+  <div class="pregunta">${enunciado}</div>
+  <form>
+    <div class="respuesta"><input type="radio" name="respuesta" value="a"><label>${respuesta_correcta}</label></div>
+    <div class="respuesta"><input type="radio" name="respuesta" value="b"><label>${opciones[0]}</label></div>
+    <div class="respuesta"><input type="radio" name="respuesta" value="c"><label>${opciones[1]}</label></div>
+    <div class="respuesta"><input type="radio" name="respuesta" value="d"><label>${opciones[2]}</label></div>
+    <input type="button" id="boton" value="Verificar" onclick="verificarRespuesta('respuesta', '${respuesta_correcta}', 'mensaje')">
+  </form>
+`;
+}*/
+function cargarProblemas(){
+  fetch('problemas.csv') // Cambia "problemas.csv" por la ruta a tu archivo CSV
+    .then(response => response.text())
+    .then(data => {
+      const problemas = data.split('\n').map(row => row.split(','));
+      const problemaAleatorio = problemas[Math.floor(Math.random() * problemas.length)];
+      const [enunciado, respuesta,o1,o2,o3,o4,o5] = problemaAleatorio;
+      const problemaDiv = document.getElementById('problema');
+      problemaDiv.innerHTML = `
+	  	<p id="timer">Tiempo transcurrido: 00:00:00</p>
+        <div class="pregunta" id="enunciado">${enunciado}</div>
+		<div class="retroalimentacion" id="mensaje"></div>
+        <form>
+          <div class="respuesta"><input type="radio" name="respuesta" value=${o1}><label>${o1}</label></div>
+          <div class="respuesta"><input type="radio" name="respuesta" value=${o2}><label>${o2}</label></div>
+          <div class="respuesta"><input type="radio" name="respuesta" value=${o3}><label>${o3}</label></div>
+          <div class="respuesta"><input type="radio" name="respuesta" value=${o4}><label>${o4}</label></div>
+          <div class="respuesta"><input type="radio" name="respuesta" value=${o5}><label>${o5}</label></div>
+          <input type="button" id="boton" value="Verificar" onclick="verificarRespuesta('${respuesta}','mensaje')">
+		  <input type="button" id="cron" value="Cronometro" onclick="cronometro('cron','timer')">
+        </form>
+      `;
+      MathJax.typeset([document.getElementById('enunciado')]);
+    });
+}
